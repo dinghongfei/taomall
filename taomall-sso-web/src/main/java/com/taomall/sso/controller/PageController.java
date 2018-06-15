f5dad21fb090534af144a9a84dc7252d185730c5
dinghongfei <dhf0710@163.com>
2018-06-15 20:47:10 +0800

    登录页面增加回调url
    跳转登录页面时，model上重定向url地址，在登录系统后，如果有重定向url，就回重定向到此url去

--- taomall-sso-web/src/main/java/com/taomall/sso/controller/PageController.java
+++ taomall-sso-web/src/main/java/com/taomall/sso/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.taomall.sso.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,7 +13,8 @@ public class PageController {
     }
 
     @RequestMapping("/page/login")
-    public String showLogin(){
+    public String showLogin(String url, Model model){
+        model.addAttribute("redirect", url);
         return "login";
     }
 
